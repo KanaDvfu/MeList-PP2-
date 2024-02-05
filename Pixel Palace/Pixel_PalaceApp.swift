860bd5f522f0ef129bda82cde9162ea998d6bf02
CMKana <kanashkin.ed@students.dvfu.ru>
2024-02-05 16:40:56 +1000

    Initial Commit

---
+++ Pixel Palace/Pixel_PalaceApp.swift
@@ -0,0 +1,20 @@
+//
+//  Pixel_PalaceApp.swift
+//  Pixel Palace
+//
+//  Created by Евгений Канашкин on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_PalaceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
